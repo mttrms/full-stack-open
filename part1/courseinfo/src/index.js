877ea8a9b3629e83a1bac100129842638c428d1b
Matt Ramos <mattramos@gmail.com>
2020-02-22 18:23:23 -0500

    Complete 1.3 & 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,26 +3,26 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const course = 'Half stack application development';
-  const courseContents = {
-    partOne: {
+  const parts = [
+    {
       name: 'Fundamentals of React',
       exercises: 10
     },
-    partTwo: {
+    {
       name: 'Using props to pass data',
       exercises: 7 
     },
-    partThree: {
+    {
       name: 'State of a component',
       exercises: 14
     }
-  }
+  ]
 
   return (
     <div>
       <Header name={course} />
-      <Content courseContents={courseContents} />
-      <Total courseContents={courseContents} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 };
@@ -32,15 +32,14 @@ const Header = ({ name }) => (
 );
 
 
-const Content = ({ courseContents }) => (
+const Content = ({ parts }) => (
   <>
-    { Object.values(courseContents).map((course, i) => <p key={i}>{course.name} {course.exercises}</p>) }
+    { parts.map((course, i) => <p key={i}>{course.name} {course.exercises}</p>) }
   </>
 );
 
-const Total = ({ courseContents }) => {
-  const sum = Object.values(courseContents).reduce((exerciseCount, course) =>
-    exerciseCount + course.exercises, 0)
+const Total = ({ parts }) => {
+  const sum = parts.reduce((exerciseCount, course) => exerciseCount + course.exercises, 0)
   return (
     <p>Number of exercises {sum}</p>
   );
