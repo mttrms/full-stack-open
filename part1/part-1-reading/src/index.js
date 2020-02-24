c368a0faf7eb23a6cdbec024f3bec87c2be4addb
Matt Ramos <mattramos@gmail.com>
2020-02-23 21:11:01 -0500

    useState reading

--- part1/part-1-reading/src/index.js
+++ part1/part-1-reading/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = ({ name, age }) => {
@@ -12,6 +12,22 @@ const Hello = ({ name, age }) => {
   )
 }
 
+const Counter = () => {
+  const [ counter, setCounter ] = useState(0);
+  const increaseByOne = () => setCounter(counter + 1);
+  const setToZero = () => setCounter(0);
+  const setToValue = value => setCounter(value);
+
+  return (
+    <div>
+      { counter }
+      <br />
+      <button onClick={() => setToValue(counter + 1)}>+1</button>
+      <button onClick={() => setToValue(0)}>Reset</button>
+    </div>
+  )
+}
+
 const App = () => {
   const name = "Matt"
   const age = "100"
@@ -20,6 +36,7 @@ const App = () => {
       <p>Hello there</p>
       <Hello name="You" age="0" />
       <Hello name={name} age={age} />
+      <Counter />
     </>
   )
 }
