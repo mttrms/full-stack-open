1f935af44a0afd6b6ed60d03c7af47efe9d1f0b4
Matt Ramos <mattramos@gmail.com>
2020-02-26 00:25:31 -0500

    Complete reading + 1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,11 +5,37 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const feedback = {
+    good,
+    neutral,
+    bad
+  };
 
   return (
-    <div>code incoming</div>
-  )
-}
+    <>
+      <h1>Give Feedback</h1>
+      <Button onClick={() => setGood(good + 1)} text='Good' />
+      <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
+      <Button onClick={() => setBad(bad + 1)} text='Bad' />
+      <Stats feedback={feedback} />
+    </>
+  );
+};
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>{text}</button>
+);
+
+const Stats = ({feedback}) => (
+  <>
+    <h2>Statistics</h2>
+    <ul>
+      <li>Good: { feedback.good }</li>
+      <li>Neutral: { feedback.neutral }</li>
+      <li>Bad: { feedback.bad }</li>
+    </ul>
+  </>
+);
 
 ReactDOM.render(<App />,
   document.getElementById('root')
