5aae4de2b9223b91b5551f5a6ad2992d36207795
Matt Ramos <mattramos@gmail.com>
2020-03-01 17:52:09 -0500

    Complete 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 
 const App = props => {
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0]);
   const randomNum = () => Math.floor(Math.random() * anecdotes.length);
+
+  const upvote = id => {
+    const copy = [...votes];
+    copy[id] += 1;
+    setVotes(copy);
+  }
 
   return (
     <div>
       {props.anecdotes[selected].text}
       <br />
+      Votes: {votes[selected]}
+      <br />
       <button onClick={() => setSelected(randomNum)}>get random</button>
+      <button onClick={() => upvote(selected)}>Vote</button>
     </div>
   )
 };
@@ -17,27 +27,21 @@ const App = props => {
 const anecdotes = [
   {
     text: 'If it hurts, do it more often',
-    votes: 0
   },
   {
     text: 'Adding manpower to a late software project makes it later!',
-    votes: 0
   },
   {
     text: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    votes: 0
   },
   {
     text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    votes: 0
   },
   {
     text: 'Premature optimization is the root of all evil.',
-    votes: 0
   },
   {
     text: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    votes: 0
   }
 ];
 
